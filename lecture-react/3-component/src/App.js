c109de59d13071301dfed94fad6135ae80e906b1
LAPTOP-0MDP6404\PABLOAIR <hmkim@pabloair.com>
2022-06-27 09:39:38 -0400

    react components 분리

---
+++ lecture-react/3-component/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "./components/Header.js";
+
+export default class App extends React.Component {
+    render() {
+        // TODO
+        return <Header title="검색" />;
+    }
+}
---
+++ lecture-react/3-component/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// TODO
+const Header = (props) => (
+    <header>
+        <h2 className="container">{props.title}</h2>
+    </header>
+);
+
+export default Header;
